f63aea10d0714c3d629250f74d0fd4ab365487f5
vishula1 <vgamaetige@gmail.com>
2022-11-10 20:47:36 +1000

    added home page

--- server.py
+++ server.py
@@ -5,28 +5,44 @@ import os
 DB_URL = os.environ.get('DATABASE_URL', 'dbname=farfetch')
 
 app = Flask(__name__)
-
+app.secret_key = os.environ.get("SECRET_KEY") 
 @app.route('/')
-def index():
+def homepage():
+    
+    return render_template('base.html')
+
+@app.route('/shop')
+def shop():
     conn = psycopg2.connect(DB_URL)
     cur = conn.cursor()
-    cur.execute('SELECT id, name, price, image_url price FROM clothes')
+    cur.execute('SELECT id, name, price, image_url, description price FROM clothes')
     results = cur.fetchall()
 
     product_items = []
     for row in results: 
-        id, name, price, image_url = row 
+        id, name, price, image_url, description = row 
         price = f'${price/100:.2f}'
-        product_items.append([id, name, price, image_url])
-        print('test commit')
+        product_items.append([id, name, price, image_url, description])
+        
     cur.close()
     conn.close()
     
+    return render_template('shop.html', product_items=product_items)
 
+@app.route('/blog')
+def blog():
 
-    return render_template('index.html', product_items=product_items)
-# @app.route('/about')
-# def about():
-# Test Commits 
+    return render_template('blog.html')
+@app.route('/login')
+def login():
+
+    return render_template('login.html')
+
+@app.route('/create')
+def create():
+
+    return render_template('signup.html')
+
+ 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    app.run(debug=True, port=5002)
